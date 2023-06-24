d1520c4bc6ae0105467622ea5ea9a04b109952fc
Doan Bui <bxdoan93@gmail.com>
2023-06-25 00:26:17 +0700

    support list url

--- run.sh
+++ run.sh
@@ -5,16 +5,33 @@ doc_str="Usage: ./run.sh [url] [arguments]
     url: example.com
     arguments: all arguments, default is None
     example: ./run.sh example.com
+             ./run.sh -u example.com
+             ./run.sh -f list_target.txt
+             ./run.sh -h
 "
-url=$1
-# if not have url, exit
+
+list_url=()
+# if have -f argument, use it as file name, load list url from file
+if [ "$1" = "-f" ]; then
+    # Read file into list_url variable
+    while IFS= read -r line; do
+        list_url+=("$line")
+    done < "$2"
+elif [ "$1" = "-h" ]; then
+    echo "$doc_str"
+    exit 1
+elif [ "$1" = "-u" ]; then
+    list_url+=("$1")
+else
+    list_url+=("$1")
+fi
+
 if [ $# -eq 0 ]; then
     echo "$doc_str"
     exit 1
 fi
 
 today=$(date '+%Y-%m-%d_%H:%M:%S')
-file_name_log="log/${url}_${today}.log"
 
 function print_done() {
     echo "==============" >> $file_name_log
@@ -23,13 +40,21 @@ function print_done() {
     echo "\n\n\n" >> $file_name_log
 }
 
-#docker run -it --rm -v osmws:/root/.osmedeus/workspaces j3ssie/osmedeus:latest scan -f fast -t "$url" | sed -r "$color_fmt" >> $file_name_log 2>&1
-#print_done "osmedeus"
-docker run projectdiscovery/nuclei:latest -u "$url"               | sed -r "$color_fmt" >> $file_name_log 2>&1
-print_done "nuclei"
-docker run projectdiscovery/subfinder:latest -d "$url"            | sed -r "$color_fmt" >> $file_name_log 2>&1
-print_done "subfinder"
-docker run projectdiscovery/naabu:latest -host "$url"             | sed -r "$color_fmt" >> $file_name_log 2>&1
-print_done "naabu"
-docker run projectdiscovery/katana:latest -u "$url"               | sed -r "$color_fmt" >> $file_name_log 2>&1
-print_done "katana"
+
+# Iterate through each URL in list_url array
+for url in "${list_url[@]}"
+do
+    # Perform desired action on each URL, such as curling it
+    echo "process $url"
+    file_name_log="log/${url}_${today}.log"
+    #docker run -it --rm -v osmws:/root/.osmedeus/workspaces j3ssie/osmedeus:latest scan -f fast -t "$url" | sed -r "$color_fmt" >> $file_name_log 2>&1
+    #print_done "osmedeus"  docker run projectdiscovery/nuclei:latest -u "$url"               | sed -r "$color_fmt" >> $file_name_log 2>&1
+    print_done "nuclei"
+    docker run projectdiscovery/subfinder:latest -d "$url" | sed -r "$color_fmt" >> $file_name_log 2>&1
+    print_done "subfinder"
+    docker run projectdiscovery/naabu:latest -host "$url"  | sed -r "$color_fmt" >> $file_name_log 2>&1
+    print_done "naabu"
+    docker run projectdiscovery/katana:latest -u "$url"    | sed -r "$color_fmt" >> $file_name_log 2>&1
+    print_done "katana"
+done
+
