e2971c7421320218fd1951adbf66ea3275d95a69
Doan Bui <bxdoan93@gmail.com>
2023-06-24 15:22:45 +0700

    add katana

--- run.sh
+++ run.sh
@@ -12,21 +12,24 @@ if [ $# -eq 0 ]; then
     echo "$doc_str"
     exit 1
 fi
-function print_done() {
-    echo "\n\n\n" >> $file_name_log
-    echo "==============" >> $file_name_log
-    echo "$1 done" >> $file_name_log
-    echo "==============" >> $file_name_log
-}
+
 today=$(date '+%Y-%m-%d_%H:%M:%S')
 file_name_log="log/${url}_${today}.log"
 
+function print_done() {
+    echo "==============" >> $file_name_log
+    echo "$1 done" >> $file_name_log
+    echo "==============" >> $file_name_log
+    echo "\n\n\n" >> $file_name_log
+}
 
-docker run projectdiscovery/nuclei:latest -u $url                | sed -r "$color_fmt" >> $file_name_log 2>&1
+#docker run -it --rm -v osmws:/root/.osmedeus/workspaces j3ssie/osmedeus:latest scan -f fast -t "$url" | sed -r "$color_fmt" >> $file_name_log 2>&1
+#print_done "osmedeus"
+docker run projectdiscovery/nuclei:latest -u "$url"               | sed -r "$color_fmt" >> $file_name_log 2>&1
 print_done "nuclei"
-docker run projectdiscovery/subfinder:latest -d $url             | sed -r "$color_fmt" >> $file_name_log 2>&1
+docker run projectdiscovery/subfinder:latest -d "$url"            | sed -r "$color_fmt" >> $file_name_log 2>&1
 print_done "subfinder"
-docker run projectdiscovery/naabu:latest -host $url              | sed -r "$color_fmt" >> $file_name_log 2>&1
+docker run projectdiscovery/naabu:latest -host "$url"             | sed -r "$color_fmt" >> $file_name_log 2>&1
 print_done "naabu"
-docker run projectdiscovery/katana:latest -u $url                | sed -r "$color_fmt" >> $file_name_log 2>&1
+docker run projectdiscovery/katana:latest -u "$url"               | sed -r "$color_fmt" >> $file_name_log 2>&1
 print_done "katana"
