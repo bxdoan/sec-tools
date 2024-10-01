f8b0b9ec4e1b0667014db8576ff9e3420216f206
Doan Bui <bxdoan93@gmail.com>
2024-10-01 23:50:21 +0700

    cook pass 1m 1 chunk

--- src/gg.py
+++ src/gg.py
@@ -1,5 +1,6 @@
 import json, time
 import os
+from time import time_ns
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,7 +24,7 @@ def process_account(i, file_hash_path, file_res_path):
         for idx, acc in enumerate(accounts):
             info = acc.get('email') or acc.get('nricfin') or acc.get('mobile')
             pw = acc.get('password')
-            print (f"FILE {i+1} | {idx+1}/{len(accounts)} | {acc['id']} : info={info} | PW {pw}")
+            print (f"FILE {i+1} | Pools {len(lines)} | {idx+1}/{len(accounts)} | {acc['id']} : info={info} | PW {pw}")
             res = find_password_in_file(pw, lines)
             if res:
                 print(f"Found password {info},{pw}")
@@ -39,5 +40,5 @@ if __name__ == '__main__':
         process_account(j, file_hash, file_res)
 
     print(f"Processing {len(tasks)} files")
-    time_in_mins = (time.time() - st) / 60
-    print(f"Time takes {time_in_mins}")
+    time_in_h_m_s = time.strftime('%H:%M:%S', time.gmtime(time.time() - st))
+    print(f"Time takes {time_in_h_m_s}")
